4ec7c8b3252b207e468d19b992c9b7d314ac7d43
Stone Coder <stonecodersoft@gmail.com>
2020-11-08 15:31:59 +0000

    improved how available rooms are retrieved

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -17,12 +17,38 @@ class Room extends Model
     {
         return $this->belongsTo(RoomType::class, 'room_type_id');
     }
+    
+    /**
+     * Get All available room base on room type (used by ajax call mostly)
+     * 
+     * @param string|int|null $room_type_id The type of room, default null.
+     * 
+     * @return mixed 
+     */
+    public function scopeAllAvailable($query, $room_type_id = null)
+    {
+        if (is_null($room_type_id)) {
+            return $query->where('is_available', true);
+        }
+        return $query->where('is_available', true)->where('room_type_id', $room_type_id);
+    }
 
-    public function scopeAvailable($query, $id = null)
+    /**
+     * Get All available rooms including current booked room
+     * Use for editing booked the first method don't include tenant room when retrieving rooms.
+     * This might call for a refactoring to have as single method to perform that task or not.
+     * 
+     * @param mixed $query The query builder
+     * @param string|int $room_type_id The room type.
+     * @param string|int|null $id The current tenant room id
+     * 
+     * @return mixed
+     */
+    public function scopeAvailable($query, $room_type_id, $id = null)
     {
         if (is_null($id)) {
             return $query->where('is_available', true);
         }
-        return $query->where('is_available', true)->orWhere('id', $id);
+        return $query->where('is_available', true)->where('room_type_id', $room_type_id)->orWhere('id', $id);
     }
 }
